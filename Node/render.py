1883284f4b7d646294f99aed3127744f3f5ce92d
PlumpDolphin <bkeesee@mail.com>
2021-02-24 16:27:55 -0600

    Update render.py
    Improved modularity

--- Node/render.py
+++ Node/render.py
@@ -2,14 +2,22 @@ import os
 import json
 from subprocess import call
 
-config_file = open(os.path.abspath("./node.config"), 'r')
-node_config = json.load(config_file)
-render_engine = node_config["engine"]
+class Configuration:
+    def __init__(self):
+        # config_file = open(os.path.abspath("./node.config"), 'r')
+        with open(os.path.abspath("./node.config"), 'r') as _file:
+            _config = json.load(_file)
+            self.master_path = _config["path"]
+            self.render_engine = _config["engine"]
 
-config_file = open(node_config["path"] + "master.config", 'r')
-master_config = json.load(config_file)
-blender_path = master_config["exe"]
-jobs_path = master_config["jobs"]
+        
+        # config_file = open(self.master_path + "master.config", 'r')
+        with open(self.master_path + "master.config", 'r') as _file:
+            _config = json.load(_file)
+            self.blender_path = _config["exe"]
+            self.jobs_path = _config["jobs"]
 
-command = blender_path + " -b " + "B:/Blender/playground01.blend" + " -a " + "-- --cycles-device " + render_engine
+config = Configuration()
+
+command = config.blender_path + " -b " + "B:/Blender/playground01.blend" + " -a " + "-- --cycles-device " + config.render_engine
 call(command, shell=True)
